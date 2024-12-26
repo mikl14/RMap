6868b4d2c1b77e213530b40f34bd627ba26608b2
mikl14 <mikl.eli2002@gmail.com>
2024-12-26 14:52:00 +0300

    Добавлена работа с базой psql изменена цветовая схема

--- main.R
+++ main.R
@@ -1,55 +1,61 @@
 library(leaflet)
 library(dplyr)
 library(sf)
+library(RPostgres)
+library(DBI)
+
+con <- dbConnect(RPostgres::Postgres(), 
+                 dbname = "map",
+                 host = "localhost",
+                 port = 5432,
+                 user = "postgres",
+                 password = "schef2002")
+
+# Выполнение SQL-запроса
+query <- "select code, count(*) from regions_all JOIN map_regions_codes ON LEFT(regions_all.okatof,2) = map_regions_codes.id JOIN coffers2024.yan_september_15_10 ON coffers2024.yan_september_15_10.inn = regions_all.inn group by code"
+result <- dbGetQuery(con, query)
 
 
-source("shpSaveData.R")
-
-# Параметры для функции
-shapefile_path <- "admin_4.shp"
-data_csv_path <- "reg.csv"
-
-# Загружаем данные о регионах (например, из GeoJSON файла или shapefile)
-# Здесь предполагается, что у вас есть файл с геоданными регионов
 regions <- st_read("admin_4.shp")
 
 
-# Чтение шейп-файла
-shapefile <- st_read(shapefile_path)
+shapefile_with_count <- regions %>%
+  left_join(result, by = c("ISO3166.2" = "code"))
 
-# Чтение данных о популяции из CSV файла
-read_data <- read.csv2(data_csv_path)
+shapefile_with_count <- shapefile_with_count %>%
+  filter(!is.na(count))
 
-shapefile_with_population <- shapefile %>%
-  left_join(read_data, by = c("ref" = "id"))
-
-
-shapefile_with_population$population <- as.numeric(shapefile_with_population$data)
+shapefile_with_count$count <- as.numeric(shapefile_with_count$count)
 
 # Проверка на наличие значений
-if (length(unique(shapefile_with_population$population)) > 0) {
+if (length(unique(shapefile_with_count$count)) > 0) {
   # Определяем палитру для цветов на основе населения
-  pal <- colorNumeric(palette = "YlOrRd", domain = shapefile_with_population$population)
+  # Установка палитры для цветового отображения на основе значений count
+  bins <- c(0,20000, 50000, 100000, 300000, 500000, 800000,1000000,1500000)
+  pal <- colorBin(palette = "Blues", domain = shapefile_with_count$count , bins=bins, na.color = "transparent")
   
   # Создаем интерактивную карту
-  map <- leaflet(data = shapefile_with_population) %>%
+  map <- leaflet(data = shapefile_with_count) %>%
     addTiles() %>%  # Добавляем базовый слой карты
-    setView(lng = mean(st_coordinates(shapefile_with_population)[,1]), lat = mean(st_coordinates(shapefile_with_population)[,2]), zoom = 6) %>%
-    addPolygons(fillColor = ~pal(data),  # Задаем цвет заливки на основе населения
+    setView(lng = mean(st_coordinates(shapefile_with_count)[,1]), 
+            lat = mean(st_coordinates(shapefile_with_count)[,2]), 
+            zoom = 6) %>%
+    addPolygons(fillColor = ~pal(count),  # Задаем цвет заливки на основе count
                 weight = 1,
                 opacity = 1,
                 color = 'white',
                 dashArray = '3',
                 fillOpacity = 0.7,
                 highlightOptions = highlightOptions(weight = 2, color = 'black', fillOpacity = 0.7),
-                label = ~paste(name, "<br>Индекс плешивых собак 2024г: ", data),  # Всплывающая подсказка
+                label = ~paste(name, "Хозяйствующие субъекты платившие налоги в период с 01.2024 по 09.2024: ", count),  # Всплывающая подсказка
                 labelOptions = labelOptions(style = list('font-weight' = 'normal', padding = '3px 8px'),
                                             textsize = '15px',
                                             direction = 'auto')) %>%
-    addLegend(pal = pal, values = ~data, opacity = 0.7, title = "Индекс плешивых собак 2024г", position = "bottomright")
+    addLegend(pal = pal, values = ~count, opacity = 0.7, title = "Хозяйствующие субъекты платившие налоги в период с 01.2024 по 09.2024: ", position = "bottomright")
   
   # Отображаем карту
   map
+  
 } else {
   print("Нет доступных данных для отображения.")
 }
